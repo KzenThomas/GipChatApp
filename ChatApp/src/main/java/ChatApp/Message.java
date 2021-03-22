a9dd7ed8811dbae389cdf465a6612089cbc2b1f5
KzenThomas <59644019+KzenThomas@users.noreply.github.com>
2021-03-22 15:07:33 +0100

    Update Message.java

--- ChatApp/src/main/java/ChatApp/Message.java
+++ ChatApp/src/main/java/ChatApp/Message.java
@@ -1,7 +1,59 @@
 package ChatApp;
 
-public class Message {
-String name;
-String messagetext;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-}
+@Entity(name = "Messages")
+	public class Messages {
+		
+		@Id
+		@GeneratedValue
+		@Column(name = "id")
+		private Integer id = 0;
+		
+		@Column(name = "name")
+		private String name;
+		
+		@Column(name = "message")
+		private String message;
+		
+
+		public Integer getid() {
+			return id;
+		}
+		
+		public String getname() {
+			return name;
+		}
+		
+		public String getmessage() {
+			return message;
+		}
+		
+		public void setid(Integer id) {
+			this.id = id;
+		}
+		
+		public void setname(String name) {
+			this.name = name;
+		}
+		
+		public void setmessage(String message) {
+			this.message = message;
+		}
+		
+		public Messages() {
+			
+		}
+		
+		public Messages(String text) {
+			this.message = text;
+			this.name = "thomas";
+		}		
+	}
+
+
+
+
